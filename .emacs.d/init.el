c09466010fbc00ed89f4dd0889e0014a7161c6cc
Tomas Cernaj <tomas@cernaj.de>
2016-07-03 21:57:48 +0200

    moved some more configs to custom; switched icomplete for ido

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,18 +10,6 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
-;;
-;; own configurations
-;;
-
-(if window-system
-    (tool-bar-mode -1))
-(menu-bar-mode -1)
-
 ;; enable Winner mode: Cycle window configuration history by 'C-c
 ;; left' / 'C-c right'
 (winner-mode t)
-
-;; enable Icomplete mode: In minibuffer, complete with 'C-j', cycling
-;; with 'C-.', 'C-,'
-(icomplete-mode t)
