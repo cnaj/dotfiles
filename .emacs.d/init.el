311b2483d8ae8e55de56f34e04afa28c375455d2
Tomas Cernaj <tomas@cernaj.de>
2016-07-09 10:48:03 +0200

    General configuration according to http://manenko.com/2016/03/01/setup-emacs-for-rust-development.html

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,3 +16,14 @@
 
 ;; enable smartparens
 (require 'smartparens-config)
+
+;; on macOS, graphical Emacs doesn't run with shell environment, so
+;; copy PATH etc. from shell.
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; Changes all yes/no questions to y/n
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; configure use of flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
